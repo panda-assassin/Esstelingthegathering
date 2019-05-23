522e4f3ed85c89c5732d52353e2ca884c9c5a5d8
EricVeen <ericveen@hotmail.nl>
2019-05-23 10:42:53 +0200

    package

--- AndroidCode2/app/src/main/java/com/example/androidcode/MainActivity.java
+++ AndroidCode2/app/src/main/java/com/example/androidcode/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.androidcode.DataBase.DatabaseHelper;
+
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
