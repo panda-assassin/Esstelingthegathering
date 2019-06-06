4f320b13def1f0d5e68e6fa728733b4067e20a84
EricVeen <ericveen@hotmail.nl>
2019-06-06 12:27:05 +0200

    Auto stash before merge of "Development" and "origin/Development"

--- AndroidCode2/app/src/main/java/com/example/androidcode/DataBase/DatabaseHelper.java
+++ AndroidCode2/app/src/main/java/com/example/androidcode/DataBase/DatabaseHelper.java
@@ -18,32 +18,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    // Database Version
-    private static final int DATABASE_VERSION = 1;
-
     // Database Name
-    private static final String DATABASE_NAME = "userDB.db";
+    private static final String DATABASE_NAME = "userdb.db";
 
     // User table name
-    private static final String TABLE_USER = "users";
+    private static final String TABLE_NAME = "users";
 
     // User Table Columns names
     private static final String COLUMN_USER_ID = "ID";
-    private static final String COLUMN_USER_NAME = "Username";
-    private static final String COLUMN_USER_EMAIL = "Email";
-    private static final String COLUMN_USER_PASSWORD = "Password";
+    private static final String COLUMN_USER_NAME = "username";
+    private static final String COLUMN_USER_EMAIL = "email";
+    private static final String COLUMN_USER_PASSWORD = "password";
 
     // create table sql query
-    private String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USER + "("
-            + COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_USER_NAME + " TEXT,"
-            + COLUMN_USER_EMAIL + " TEXT," + COLUMN_USER_PASSWORD + " TEXT" + ")";
+    private String CREATE_USER_TABLE = "CREATE TABLE registeruser (ID INTEGER PRIMARY KEY AUTOINCREMENT,email TEXT,username TEXT, password TEXT )";
 
     // drop table sql query
-    private String DROP_USER_TABLE = "DROP TABLE IF EXISTS " + TABLE_USER;
+    private String DROP_USER_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
 
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
@@ -63,65 +58,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     }
 
-    public void addUser(User user) {
-        SQLiteDatabase db = this.getWritableDatabase();
+    public void addUser(String user, String password, String email) {
+        SQLiteDatabase db = getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(COLUMN_USER_NAME, user.getName());
-        values.put(COLUMN_USER_EMAIL, user.getEmail());
-        values.put(COLUMN_USER_PASSWORD, user.getPassword());
+        values.put(COLUMN_USER_NAME, user);
+        values.put(COLUMN_USER_EMAIL, email);
+        values.put(COLUMN_USER_PASSWORD, password);
 
         // Inserting Row
-        db.insert(TABLE_USER, null, values);
+        db.insert(TABLE_NAME, null, values);
         db.close();
-    }
-
-    public List<User> getAllUser() {
-        // array of columns to fetch
-        String[] columns = {
-                COLUMN_USER_ID,
-                COLUMN_USER_EMAIL,
-                COLUMN_USER_NAME,
-                COLUMN_USER_PASSWORD
-        };
-        // sorting orders
-        String sortOrder =
-                COLUMN_USER_NAME + " ASC";
-        List<User> userList = new ArrayList<User>();
-
-        SQLiteDatabase db = this.getReadableDatabase();
-
-        // query the user table
-        Cursor cursor = db.query(TABLE_USER, //Table to query
-                columns,    //columns to return
-                null,        //columns for the WHERE clause
-                null,        //The values for the WHERE clause
-                null,       //group the rows
-                null,       //filter by row groups
-                sortOrder); //The sort order
-
-
-        // Traversing through all rows and adding to list
-        if (cursor.moveToFirst()) {
-            do {
-                User user = new User();
-                user.setId(Integer.parseInt(cursor.getString(cursor.getColumnIndex(COLUMN_USER_ID))));
-                user.setName(cursor.getString(cursor.getColumnIndex(COLUMN_USER_NAME)));
-                user.setEmail(cursor.getString(cursor.getColumnIndex(COLUMN_USER_EMAIL)));
-                user.setPassword(cursor.getString(cursor.getColumnIndex(COLUMN_USER_PASSWORD)));
-                // Adding user record to list
-                userList.add(user);
-            } while (cursor.moveToNext());
-        }
-        cursor.close();
-        db.close();
-
-        // return user list
-        return userList;
     }
 
     public void updateUser(User user) {
-        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteDatabase db = getWritableDatabase();
 
         ContentValues values = new ContentValues();
         values.put(COLUMN_USER_NAME, user.getName());
@@ -129,67 +80,33 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         values.put(COLUMN_USER_PASSWORD, user.getPassword());
 
         // updating row
-        db.update(TABLE_USER, values, COLUMN_USER_ID + " = ?",
+        db.update(TABLE_NAME, values, COLUMN_USER_ID + " = ?",
                 new String[]{String.valueOf(user.getId())});
         db.close();
     }
 
     public void deleteUser(User user) {
-        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteDatabase db = getWritableDatabase();
         // delete user record by id
-        db.delete(TABLE_USER, COLUMN_USER_ID + " = ?",
+        db.delete(TABLE_NAME, COLUMN_USER_ID + " = ?",
                 new String[]{String.valueOf(user.getId())});
         db.close();
     }
 
-    public boolean checkUser(String email) {
-
+    public boolean checkUser(String username) {
         // array of columns to fetch
         String[] columns = {
                 COLUMN_USER_ID
         };
-        SQLiteDatabase db = this.getReadableDatabase();
-
+        SQLiteDatabase db = getReadableDatabase();
         // selection criteria
-        String selection = COLUMN_USER_EMAIL + " = ?";
-
-        // selection argument
-        String[] selectionArgs = {email};
-
-        // query user table with condition
-        Cursor cursor = db.query(TABLE_USER, //Table to query
-                columns,                    //columns to return
-                selection,                  //columns for the WHERE clause
-                selectionArgs,              //The values for the WHERE clause
-                null,                       //group the rows
-                null,                      //filter by row groups
-                null);                      //The sort order
-        int cursorCount = cursor.getCount();
-        cursor.close();
-        db.close();
-
-        if (cursorCount > 0) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public boolean checkUser(String email, String password) {
-
-        // array of columns to fetch
-        String[] columns = {
-                COLUMN_USER_ID
-        };
-        SQLiteDatabase db = this.getReadableDatabase();
-        // selection criteria
-        String selection = COLUMN_USER_EMAIL + " = ?" + " AND " + COLUMN_USER_PASSWORD + " = ?";
+        String selection = COLUMN_USER_NAME + " = ?";
 
         // selection arguments
-        String[] selectionArgs = {email, password};
+        String[] selectionArgs = {username};
 
         // query user table with conditions
-        Cursor cursor = db.query(TABLE_USER, //Table to query
+        Cursor cursor = db.query(TABLE_NAME, //Table to query
                 columns,                    //columns to return
                 selection,                  //columns for the WHERE clause
                 selectionArgs,              //The values for the WHERE clause
@@ -201,6 +118,41 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         cursor.close();
         db.close();
+
+        if (cursorCount > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean checkUser(String username, String password) {
+
+        // array of columns to fetch
+        String[] columns = {
+                COLUMN_USER_ID
+        };
+        SQLiteDatabase db = getReadableDatabase();
+        // selection criteria
+        String selection = COLUMN_USER_NAME + " = ?" + " and " + COLUMN_USER_PASSWORD + " = ?";
+
+        // selection arguments
+        String[] selectionArgs = {username, password};
+
+        // query user table with conditions
+        Cursor cursor = db.query(TABLE_NAME, //Table to query
+                columns,                    //columns to return
+                selection,                  //columns for the WHERE clause
+                selectionArgs,              //The values for the WHERE clause
+                null,                       //group the rows
+                null,                       //filter by row groups
+                null);                      //The sort order
+
+        int cursorCount = cursor.getCount();
+
+        cursor.close();
+        db.close();
+
         if (cursorCount > 0) {
             return true;
         }
--- AndroidCode2/app/src/main/java/com/example/androidcode/StartUp/MainActivity.java
+++ AndroidCode2/app/src/main/java/com/example/androidcode/StartUp/MainActivity.java
@@ -1,39 +1,56 @@
 package com.example.androidcode.StartUp;
 
 import android.content.Intent;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.androidcode.DataBase.DatabaseHelper;
 import com.example.androidcode.DataBase.InputValidation;
-import com.example.androidcode.DataBase.DatabaseHelper;
-import com.example.androidcode.QueueList.CheckInActivity;
 import com.example.androidcode.R;
 
 public class MainActivity extends AppCompatActivity {
-    private final AppCompatActivity activity = MainActivity.this;
 
-    private EditText inputTextEmail;
-    private EditText inputTextPassword;
-
+    private EditText username;
+    private EditText password;
     private Button loginButton;
-
     private Button registerButton;
-
-    private InputValidation inputValidation;
-    private DatabaseHelper databaseHelper;
+    private DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = new DatabaseHelper(this);
+        username = (EditText)findViewById(R.id.loginUsername);
+        password = (EditText)findViewById(R.id.loginPassword);
+        loginButton = (Button)findViewById(R.id.logInButton);
+        registerButton = (Button)findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString().trim();
+                String pwd = password.getText().toString().trim();
+                if (db.checkUser(user, pwd)) {
+                    Intent loginIntent = new Intent(MainActivity.this, HomeScreenActivity.class);
+                    startActivity(loginIntent);
+                } else {
+                    Toast.makeText(MainActivity.this, R.string.error_valid_username_password, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
-
-
-
 }
