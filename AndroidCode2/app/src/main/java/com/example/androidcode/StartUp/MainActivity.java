c13cf1d4cc2813891b66779a4ce4d622443000a5
EricVeen <ericveen@hotmail.nl>
2019-06-07 01:08:05 +0200

    Auto stash before merge of "Development" and "origin/Development"

--- AndroidCode2/app/src/main/java/com/example/androidcode/StartUp/MainActivity.java
+++ AndroidCode2/app/src/main/java/com/example/androidcode/StartUp/MainActivity.java
@@ -53,5 +53,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString().trim();
+                String pwd = password.getText().toString().trim();
+                if (db.checkUser(user, pwd)) {
+                    Intent loginIntent = new Intent(MainActivity.this, HomeScreenActivity.class);
+                    startActivity(loginIntent);
+                } else {
+                    Toast.makeText(MainActivity.this, R.string.error_valid_username_password, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
